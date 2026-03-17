2f7d7053f90b3498d5e0613ad08de1533c4150e3
Sunkara Venkata Karthik Sai <venkatakarthiksai.s@gmail.com>
2026-03-18 03:04:56 +0530

    changes

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,10 +1,9 @@
-import { useTheme } from "next-themes"
 import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const theme = typeof document !== "undefined" ? (document.documentElement.classList.contains("dark") ? "dark" : "light") : "system"
 
   return (
     <Sonner
